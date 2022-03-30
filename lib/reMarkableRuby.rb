0aafdd131b772c79974f0b51aefbf5f361d4a211
Jordan Streiff <jordan.streiff@gmail.com>
2022-03-30 10:20:33 -0400

    Initial commit

---
+++ lib/reMarkableRuby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "reMarkableRuby/version"
+
+module ReMarkableRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/reMarkableRuby/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ReMarkableRuby
+  VERSION = "0.1.0"
+end
