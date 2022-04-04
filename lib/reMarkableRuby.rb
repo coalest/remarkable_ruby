9643af8f47a6c36fe15f9f432bc49c330371bc3b
Jordan Streiff <jordan.streiff@gmail.com>
2022-04-04 10:02:11 -0400

    Adds token refresh

--- lib/reMarkableRuby.rb
+++ lib/reMarkableRuby.rb
@@ -5,25 +5,37 @@ require "faraday"
 require "faraday/net_http"
 require "securerandom"
 require "json"
-require "pry"
 
 module ReMarkableRuby
   class Error < StandardError; end
 
   class Client
+    APP_URL = "https://webapp-production-dot-remarkable-production.appspot.com/"
+
     def initialize(one_time_code)
       Faraday.default_adapter = :net_http
 
       @uuid = SecureRandom.uuid
       @auth_token = authenticate(one_time_code)
       @storage_uri = fetch_storage_uri
+      refresh_token(@auth_token)
     end
 
     def files
-      conn = Faraday.new(url: "https://#{@storage_uri}/",
-                         headers: {'Authorization: Bearer' => "#{auth_token}"})
+      conn = Faraday.new(
+        url: "https://#{@storage_uri}/",
+        headers: {'Authorization: Bearer' => "#{@auth_token}"}
+      )
       response = conn.get("document-storage/json/2/docs")
-      binding.pry
+      response.body
+    end
+
+
+    def refresh_token(auth_token)
+      conn = Faraday.new(url: APP_URL,
+                         headers: {'Authorization: Bearer' => auth_token})
+      response = conn.post("token/json/2/user/new", "", "Content-Type" => "application/json")
+      
       response.body
     end
 
@@ -31,8 +43,10 @@ module ReMarkableRuby
     attr_reader :auth_token
 
     def authenticate(one_time_code)
-      conn = Faraday.new(url: 'https://webapp-production-dot-remarkable-production.appspot.com/',
-                  headers: {'Authorization: Bearer' => ""})     
+      conn = Faraday.new(
+        url: APP_URL,
+        headers: {'Authorization: Bearer' => ""}
+      )
       payload = { deviceDesc: "desktop-macos",
                   code: one_time_code,
                   deviceID: @uuid }.to_json
@@ -50,5 +64,4 @@ module ReMarkableRuby
     end
   end
 end
-
 
