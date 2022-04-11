75df0c7b65af37e0737357b136ea27f370dbcba8
coalest <c.streiff@pm.me>
2022-04-11 10:47:15 +0200

    rubocop style edits

--- lib/remarkable_ruby.rb
+++ lib/remarkable_ruby.rb
@@ -15,12 +15,13 @@ module ReMarkableRuby
   class Client
     APP_URL = "https://webapp-production-dot-remarkable-production.appspot.com/"
     SERVICE_DISCOVERY_URL = "https://service-manager-production-dot-remarkable-production.appspot.com/"
+    SERVICE_DISCOVERY_ENDPOINT = "service/json/1/document-storage?"\
+      "environment=production&group=auth0%7C5a68dc51cb30df3877a1d7c4&apiVer=2"
 
     def initialize(one_time_code = nil)
       tokens = Config.load_tokens || {}
-      
-      @device_token = tokens['devicetoken'] || authenticate(one_time_code)
-      @user_token = tokens['usertoken'] || refresh_token
+      @device_token = tokens["devicetoken"] || authenticate(one_time_code)
+      @user_token = tokens["usertoken"] || refresh_token
       Config.save(device_token: @device_token, user_token: @user_token)
 
       @storage_uri = fetch_storage_uri
@@ -29,7 +30,7 @@ module ReMarkableRuby
     def files
       conn = Faraday.new(
         url: "https://#{@storage_uri}?withBlob=true/",
-        headers: {'Authorization' => "Bearer #{@user_token}"}
+        headers: { "Authorization" => "Bearer #{@user_token}" }
       )
       response = conn.get("document-storage/json/2/docs")
       JSON.parse(response.body)
@@ -40,7 +41,7 @@ module ReMarkableRuby
     def authenticate(one_time_code)
       conn = Faraday.new(
         url: APP_URL,
-        headers: {'Authorization' => "Bearer "}
+        headers: { "Authorization" => "Bearer " }
       )
       payload = { deviceDesc: "desktop-macos",
                   code: one_time_code,
@@ -52,18 +53,17 @@ module ReMarkableRuby
 
     def fetch_storage_uri
       conn = Faraday.new(url: SERVICE_DISCOVERY_URL,
-                         headers: {'Authorization: Bearer' => "#{@device_token}"})     
-      response = conn.get('service/json/1/document-storage?environment=production&group=auth0%7C5a68dc51cb30df3877a1d7c4&apiVer=2')
+                         headers: { "Authorization: Bearer" => @device_token.to_s })
+      response = conn.get(SERVICE_DISCOVERY_ENDPOINT)
       response_body = JSON.parse(response.body)
       response_body["Host"]
     end
 
     def refresh_token
       conn = Faraday.new(url: APP_URL,
-                         headers: {'Authorization' => "Bearer #{@device_token}"})
+                         headers: { "Authorization" => "Bearer #{@device_token}" })
       response = conn.post("token/json/2/user/new", "", "Content-Type" => "application/json")
       response.body
     end
   end
 end
-
