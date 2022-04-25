40ecb0d9a4b266337c44fff2b1ff76931945fb96
coalest <c.streiff@pm.me>
2022-04-25 13:52:50 +0200

    Wrap config class in main module

--- lib/remarkable_ruby/config.rb
+++ lib/remarkable_ruby/config.rb
@@ -1,21 +1,23 @@
 require 'yaml'
 
-module Config
-  HOME_DIR = Dir.home
-  CONFIG_FILE_PATH = HOME_DIR + '/.rmapi'
+module RemarkableRuby
+  class Config
+    HOME_DIR = Dir.home
+    CONFIG_FILE_PATH = HOME_DIR + '/.rmapi'
 
-  def self.load_tokens
-    return unless File.exists?(CONFIG_FILE_PATH)
+    def self.load_tokens
+      return unless File.exists?(CONFIG_FILE_PATH)
 
-    YAML.load_file(CONFIG_FILE_PATH)
-  end
+      YAML.load_file(CONFIG_FILE_PATH)
+    end
 
-  def self.save(device_token: nil, user_token: nil)
-    FileUtils.touch(CONFIG_FILE_PATH) unless File.exists?(CONFIG_FILE_PATH)
+    def self.save(device_token: nil, user_token: nil)
+      FileUtils.touch(CONFIG_FILE_PATH) unless File.exists?(CONFIG_FILE_PATH)
 
-    config_hash = YAML::load_file(CONFIG_FILE_PATH) || {}
-    config_hash['devicetoken'] = device_token if device_token
-    config_hash['usertoken'] = user_token if user_token
-    File.write(CONFIG_FILE_PATH, config_hash.to_yaml)
+      config_hash = YAML::load_file(CONFIG_FILE_PATH) || {}
+      config_hash['devicetoken'] = device_token if device_token
+      config_hash['usertoken'] = user_token if user_token
+      File.write(CONFIG_FILE_PATH, config_hash.to_yaml)
+    end
   end
 end
