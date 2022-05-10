3c18cd261f0a522eec883517242185ef8902aa11
coalest <c.streiff@pm.me>
2022-05-10 12:45:13 +0200

    Add response handling module

---
+++ lib/remarkable_ruby/response_handling.rb
@@ -0,0 +1,26 @@
+module RemarkableRuby
+  module ResponseHandling
+    def handle_response(response)
+      status = response.status
+      body = response.body
+
+      case status
+      when 200
+        response_body = JSON.parse(response.body).first
+        successful = response_body["Success"]
+        message = response_body["Message"]
+        raise Error, message unless successful
+      when 400
+        raise Error, "Your request was malformed. #{body}"
+      when 401
+        raise Error, "Invalid authentication credentials. #{body}"
+      when 403
+        raise Error, "You are not allowed to perform that action. #{body}"
+      when 404
+        raise Error, "No results were found for your request. #{body}"
+      end
+
+      response
+    end
+  end
+end
