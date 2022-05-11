8cde6f3f3e406aac997e63aa403c4ec939bb3a00
coalest <c.streiff@pm.me>
2022-05-11 11:21:50 +0200

    Zip#dump creates zip files for folders too

--- lib/remarkable_ruby/zip_document.rb
+++ lib/remarkable_ruby/zip_document.rb
@@ -7,14 +7,21 @@ module RemarkableRuby
       @path_to_file = item.path
     end
 
+    # TODO: Break up this method into submethods
     def dump
       uuid = item.uuid
       folder = Dir.mktmpdir(uuid)
       zip_path = Dir.tmpdir + "/#{uuid}.zip"
-      input_filenames = ["#{uuid}.content", "#{uuid}.pagedata", "#{uuid}.pdf"]
-      File.write(folder + "/#{uuid}.content", default_pdf_content.to_json)
-      File.write(folder + "/#{uuid}.pagedata", "")
-      FileUtils.cp(@path_to_file, folder + "/#{uuid}.pdf")
+      input_filenames = ["#{uuid}.content"]
+      case item.type
+      when "CollectionType"
+        File.write(folder + "/#{uuid}.content", {})
+      when "DocumentType"
+        input_filenames.push("#{uuid}.pagedata", "#{uuid}.pdf")
+        File.write(folder + "/#{uuid}.content", default_pdf_content.to_json)
+        File.write(folder + "/#{uuid}.pagedata", "")
+        FileUtils.cp(@path_to_file, folder + "/#{uuid}.pdf")
+      end
       Zip::File.open(zip_path, create: true) do |zipfile|
         input_filenames.each do |filename|
           zipfile.add(filename, File.join(folder, filename))
