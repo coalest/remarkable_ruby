b8403f5ffcec58f383990389bd3888a8508374ab
coalest <c.streiff@pm.me>
2022-05-11 11:20:10 +0200

    Add name and parent parameters for folders

--- lib/remarkable_ruby/items/folder.rb
+++ lib/remarkable_ruby/items/folder.rb
@@ -1,8 +1,11 @@
 module RemarkableRuby
   class Folder < Item
-    def initialize(attrs: nil, client: nil, path: nil)
+    def initialize(attrs: nil, client: nil, name: nil, parent: nil)
+      super(attrs: attrs, client: client)
+
+      @name = name if name
+      @parent = parent if parent
       @type = "CollectionType"
-      super
     end
   end
 end
